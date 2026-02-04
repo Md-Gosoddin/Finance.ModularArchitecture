16eb4d3ee35cd8c731186ff6faffbb2ff151fee7
Md-Gosoddin <Md.Gosoddin@gmail.com>
2026-02-04 22:20:41 +0530

    Client Entity Implemented by using DDD

---
+++ Evently.Application/src/Modules/UserModule/Evently.Module.User.Domain/Modules/ClientModules.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using BuildingBlock.Domain;
+
+namespace Evently.Module.User.Domain.Modules;
+public sealed class ClientModules : Entity
+{
+    [Key]
+    public Guid ClientGuid { get; set; }
+    public string UserName { get; set; }
+    public string Email { get; set; }
+    public string IdentityId { get; private set; }
+
+    public static ClientModules Create(Guid userId, string email)
+    {
+        var @event = new ClientModules
+        {
+            ClientGuid = userId,
+            Email = email
+        };
+
+        @event.Raise(new UserRegisteredDomainEvent(@event.ClientGuid));
+        return @event;
+    }
+}
+
+public sealed class UserRegisteredDomainEvent(Guid userId) : DomainEvent
+{
+    public Guid UserId { get; init; } = userId;
+}
