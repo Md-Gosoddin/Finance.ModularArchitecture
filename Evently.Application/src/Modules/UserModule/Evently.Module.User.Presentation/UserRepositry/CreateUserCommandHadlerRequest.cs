dd035a85d71457bdb0033ec453f553f6d3edac8e
Md-Gosoddin <Md.Gosoddin@gmail.com>
2026-02-05 19:58:31 +0530

    Evently.User.Application was installed wrong Folder. Its Moved Inside UserModule folder

--- Evently.Application/src/Modules/UserModule/Evently.Module.User.Presentation/UserRepositry/CreateUserCommandHadlerRequest.cs
+++ Evently.Application/src/Modules/UserModule/Evently.Module.User.Presentation/UserRepositry/CreateUserCommandHadlerRequest.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Routing;
+
+
 namespace Evently.Module.User.Presentation.UserRepositry;
 internal sealed class CreateUserCommandHadlerRequest : IEndpoint
 {
